97b8bc9c2b4cfe52d99c0d766738d98d6a97af28
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2021-12-23 19:04:01 +0200

    Apparently it wasn't unnecessary

--- src/main/kotlin/net/spleefx/web/SecurityConfig.kt
+++ src/main/kotlin/net/spleefx/web/SecurityConfig.kt
@@ -1,30 +1,30 @@
-//package net.spleefx.web
-//
-//import org.springframework.context.annotation.Bean
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-//import org.springframework.web.cors.CorsConfiguration
-//import org.springframework.web.cors.CorsConfigurationSource
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-//
-//@EnableWebSecurity
-//class SecurityConfig : WebSecurityConfigurerAdapter() {
-//
-//    override fun configure(http: HttpSecurity) {
-//        http.cors().and().csrf().disable().requiresChannel().anyRequest().requiresSecure()
-//            .and().authorizeRequests().antMatchers("/favicon.ico").permitAll()
-//    }
-//
-//    @Bean
-//    fun corsConfigurationSource(): CorsConfigurationSource? {
-//        val configuration = CorsConfiguration()
-//        configuration.allowedOrigins = listOf("*")
-//        configuration.allowedMethods = listOf("*")
-//        configuration.allowedHeaders = listOf("*")
-//        configuration.allowCredentials = true
-//        val source = UrlBasedCorsConfigurationSource()
-//        source.registerCorsConfiguration("/**", configuration)
-//        return source
-//    }
-//}
+package net.spleefx.web
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.cors().and().csrf().disable().requiresChannel().anyRequest().requiresSecure()
+            .and().authorizeRequests().antMatchers("/favicon.ico").permitAll()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
