2be972a01610d74d0e921c367f6c1c63ef5e6466
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2021-12-23 18:56:12 +0200

    Remove unnecessary security configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 }
 
 dependencies {
+    implementation("redis.clients:jedis:4.0.0")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
