ff594b90bfcd6aca3d4d583793cd83efe827e9c7
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2021-12-23 18:34:05 +0200

    Https attempt

--- src/main/kotlin/net/spleefx/web/SpleefX.kt
+++ src/main/kotlin/net/spleefx/web/SpleefX.kt
@@ -1,10 +1,45 @@
 package net.spleefx.web
 
+import org.apache.catalina.Context
+import org.apache.catalina.connector.Connector
+import org.apache.tomcat.util.descriptor.web.SecurityCollection
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpleefX
+class SpleefX {
+
+    @Bean
+    fun servletContainer(): ServletWebServerFactory {
+        val tomcat: TomcatServletWebServerFactory = object : TomcatServletWebServerFactory() {
+            override fun postProcessContext(context: Context) {
+                val securityConstraint = SecurityConstraint()
+                securityConstraint.userConstraint = "CONFIDENTIAL"
+                val collection = SecurityCollection()
+                collection.addPattern("/*")
+                securityConstraint.addCollection(collection)
+                context.addConstraint(securityConstraint)
+            }
+        }
+        tomcat.addAdditionalTomcatConnectors(httpConnector)
+        return tomcat
+    }
+
+    private val httpConnector: Connector
+        get() {
+            val connector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
+            connector.scheme = "http"
+            connector.port = 8080
+            connector.secure = false
+            connector.redirectPort = 443
+            return connector
+        }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<SpleefX>(*args)
